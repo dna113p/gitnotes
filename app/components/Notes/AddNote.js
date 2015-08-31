df0dc94f201481a838d74ca655b09c85f6d39b31
dna113p <djpelland@gmail.com>
2015-08-31 14:16:41 -0700

    fixed Add Note submit button

--- app/components/Notes/AddNote.js
+++ app/components/Notes/AddNote.js
@@ -18,7 +18,7 @@ var AddNote = React.createClass({
                 <form onSubmit={this.handleSubmit} >
                     <input type="text" className="form-control" ref="note" placeholder="Add New Note" />
                     <span className="input-group-btn">
-                        <button className="btn btn-default" type="button"> Submit </button>
+                        <button className="btn btn-default" type="button" onClick={this.handleSubmit}> Submit </button>
                     </span>
                 </form>
             </div>
